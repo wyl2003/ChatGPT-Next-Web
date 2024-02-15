3e4bcc3d8f063d1633c07b362cb520a11501b2ce
Wang Yile <108109393+wyl2003@users.noreply.github.com>
2024-02-16 01:05:06 +0800

    Update sidebar.tsx
    增加定价页面

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,8 @@ export function SideBar(props: { className?: string }) {
           NextChat
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          仅以小资咖啡价，开启AI新境界。微信：my_wxid_is_wxid，体验GPT-4智能、AI画图与搜索等丰富插件。
+          仅以小资咖啡价，开启AI新境界。 <br>微信：my_wxid_is_wxid，体验GPT-4智能、 <br>AI画图与搜索等丰富插件。
+            <a href="gpt-pricing.wyls.top" target="_blank">查看定价</a>
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
