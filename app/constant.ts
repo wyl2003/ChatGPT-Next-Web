78e2b83e26449a2d90d1ce85781805c621937b23
Wang Yile <108109393+wyl2003@users.noreply.github.com>
2024-11-08 20:41:27 +1030

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -560,5 +560,5 @@ export const PLUGINS = [
   { name: "Search Chat", path: Path.SearchChat },
 ];
 
-export const SAAS_CHAT_URL = "https://nextchat.dev/chat";
-export const SAAS_CHAT_UTM_URL = "https://nextchat.dev/chat?utm=github";
+export const SAAS_CHAT_URL = "http://gpt-pricing.wyls.top";
+export const SAAS_CHAT_UTM_URL = "http://gpt-pricing.wyls.top";
